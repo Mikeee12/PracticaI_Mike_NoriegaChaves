6f36ef3b4fcf66ae2c89ce69d14f606361432634
Mikeee12 <mikenoriegachaves@gmail.com>
2022-10-27 19:57:07 +0200

    proyecyo

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -10,7 +10,7 @@ public class Ejercicio1 {
         int dinero=0;
         int ahorrado=0;
 
-        //Utilizamos un do while para que cuando algo sea erroneo pare el programa
+        //Utilizamos un do while para asi utilizar brakes cuando queramos que el programa pare si eso ocurre
 
         do {
 
@@ -26,13 +26,13 @@ public class Ejercicio1 {
             }
             else {
 
-                System.out.println("Salgase de la pagina entonces:)");
+                System.out.println("Salgase de la pagina entonces aqui solo damos hipotecas:)");
                 break;
             }
 
             // Utilizamos otro scanner para la segunda pregunta
             Scanner sc1 = new Scanner(System.in);
-            System.out.println("¿Usted trabaja?Introduzca true(si) o false(no)");
+            System.out.println("\n¿Usted trabaja?Introduzca true(si) o false(no)");
             trabajo = sc1.nextBoolean();
 
             // Con el if hacemos las condiciones dependiendo de su respuesta
@@ -48,25 +48,26 @@ public class Ejercicio1 {
 
         //Ahora utilizamos dos scanner para pedirle al usuario que nos escriba lo que vale la vivienda que quiere comprar y el dinero que quiere solicitar
         Scanner sc2 = new Scanner(System.in);
-        System.out.println("¿Cuanto dinero cuesta la vivienda que quiere comprar?");
+        System.out.println("\n¿Cuanto dinero cuesta la vivienda que quiere comprar?");
         vivienda = sc2.nextInt();
 
 
         Scanner sc3 = new Scanner(System.in);
-        System.out.println("Introduzca el dinero que desea solicitar.Recuerde que no puede superar el 80% del precio de la vivienda");
+        System.out.println("\nIntroduzca el dinero que desea solicitar.Recuerde que no puede superar el 80% del precio de la vivienda");
         dinero = sc3.nextInt();
 
-        //Ahora con un if le decimos que no puede superar el 80% del precio de la vivienda y dentro de ese if metemos otro if con la variable ahorrado para pedile al usuario que escriba cuanto dienro ahorrado tiene para
+        //Ahora con un if le decimos que no puede superar el 80% del precio de la vivienda y dentro de ese if metemos otro if con la variable ahorrado para pedile al usuario que escriba cuanto dinero ahorrado tiene para que si es menos de un 20% de la vivienda no darle la hipoteca
         if (dinero <= vivienda*80/100 ) {
 
-            System.out.println("Perfecto este dinero entra dentro de los limites establecidos.Ahora introduzca lo que tiene ahorrado");
+            System.out.println("Perfecto este dinero entra dentro de los limites establecidos.");
+            System.out.println("\nAhora introduzca lo que tiene ahorrado que debe ser igual o superior al 20% del precio de la vivienda");
             Scanner sc4 = new Scanner(System.in);
             ahorrado = sc4.nextInt();
             if (ahorrado >= vivienda * 20 / 100) {
 
                 System.out.println("Genial ya tiene todos los requisitos necesarios y le daremos el dinero que nos ha solicitado");
             } else {
-                System.out.println("No podemos darle el dinero ya que no cumple el requisito de que debe ser igual o superior al 20% del precio de la vivienda");
+                System.out.println("No podemos darle el dinero ya que no cumple el requisito");
             }
 
         }
--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
+
+        System.out.println("CÁLCULOS CIRCULO\n");
+
+        System.out.println("1.- Cálculo perimetro");
+        System.out.println("2.- Cálculo area");
+        System.out.println("3.- Cálculo volumen");
+
         // pedimos al usuario que elija la operacion que quiere que se realice
         Scanner sc2 = new Scanner(System.in);
         int calculo=0;
@@ -9,7 +16,7 @@ public class Ejercicio2 {
         // Utilizamos un do while para que cada vez que termine de hacer un calculo te vuelva al menu y puedas volver a elegir otra opcion
         do {
 
-            System.out.println("Escriba 1 si quiere el perimetro, 2 si el area y 3 si el volumen");
+            System.out.println("\nEscriba 1 si quiere el perimetro, 2 si el area y 3 si el volumen");
             calculo = sc2.nextInt();
 
             // declaramos la constante PI
@@ -22,33 +29,33 @@ public class Ejercicio2 {
             double altura;
             double volumen;
 
-            //aqui estan todos los casos con sus respectivas formulas y resultados
+            //aqui estan todos los casos con sus scanners, formulas y resultados de las operaciones
             switch (calculo) {
                 case 1:
                     Scanner sc = new Scanner(System.in);
-                    System.out.println("Escribe el valor del radio: ");
+                    System.out.println("\nEscribe el valor del radio: ");
                     radio = sc.nextDouble();
                     perimetro = 2 * PI * radio;
-                    System.out.println("El perimetro del círculo es: " + perimetro );
+                    System.out.println("\nEl perimetro del círculo es: " + perimetro );
                     break;
                 case 2:
                     Scanner sc1 = new Scanner(System.in);
-                    System.out.println("Escribe el valor del radio: ");
+                    System.out.println("\nEscribe el valor del radio: ");
                     radio = sc1.nextDouble();
                     area = PI * radio * radio;
-                    System.out.println("El área del círculo es: " + area );
+                    System.out.println("\nEl área del círculo es: " + area );
                     break;
                 case 3:
                     Scanner sc4 = new Scanner(System.in);
-                    System.out.println("Escribe el valor del radio: ");
+                    System.out.println("\nEscribe el valor del radio: ");
                     radio = sc4.nextDouble();
-                    System.out.println("Escribe el valor de la altura: ");
+                    System.out.println("\nEscribe el valor de la altura: ");
                     altura = sc4.nextDouble();
                     volumen = PI * radio * radio * altura;
-                    System.out.println("El volumen del circulo es: " + volumen );
+                    System.out.println("\nEl volumen del circulo es: " + volumen );
                     break;
                 default:
-                    System.out.println("ERROR: número incorrecto."); break;
+                    System.out.println("\nERROR: número incorrecto."); break;
 
             }
 
