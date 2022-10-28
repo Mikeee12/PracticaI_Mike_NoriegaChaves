9eafe45daade0d9c79eb249ce3213fac83484689
MikeNoriegaC <mikenoriegachaves@gmail.com>
2022-10-28 13:05:16 +0200

    proyecyo

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -11,57 +11,63 @@ public class Ejercicio2 {
 
         // pedimos al usuario que elija la operacion que quiere que se realice
         Scanner sc2 = new Scanner(System.in);
-        int calculo=0;
+        int calculo = 0;
 
         // Utilizamos un do while para que cada vez que termine de hacer un calculo te vuelva al menu y puedas volver a elegir otra opcion
         do {
 
             System.out.println("\nEscriba 1 si quiere el perimetro, 2 si el area y 3 si el volumen");
             calculo = sc2.nextInt();
+            //este if hace que si pones un numero que no sea 1,2 y 3 no te deje continuar
+            if (calculo < 4) {
 
-            // declaramos la constante PI
-            final double PI = 3.146;
 
-            // declaramos las variables
-            double area;
-            double radio;
-            double perimetro;
-            double altura;
-            double volumen;
+                // declaramos la constante PI
 
-            //aqui estan todos los casos con sus scanners, formulas y resultados de las operaciones
-            switch (calculo) {
-                case 1:
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println("\nEscribe el valor del radio: ");
-                    radio = sc.nextDouble();
-                    perimetro = 2 * PI * radio;
-                    System.out.println("\nEl perimetro del círculo es: " + perimetro );
-                    break;
-                case 2:
-                    Scanner sc1 = new Scanner(System.in);
-                    System.out.println("\nEscribe el valor del radio: ");
-                    radio = sc1.nextDouble();
-                    area = PI * radio * radio;
-                    System.out.println("\nEl área del círculo es: " + area );
-                    break;
-                case 3:
-                    Scanner sc4 = new Scanner(System.in);
-                    System.out.println("\nEscribe el valor del radio: ");
-                    radio = sc4.nextDouble();
-                    System.out.println("\nEscribe el valor de la altura: ");
-                    altura = sc4.nextDouble();
-                    volumen = PI * radio * radio * altura;
-                    System.out.println("\nEl volumen del circulo es: " + volumen );
-                    break;
-                default:
-                    System.out.println("\nERROR: número incorrecto."); break;
+                final double PI = 3.146;
 
+                // declaramos las variables
+                double area = 0;
+                double radio = 0;
+                double perimetro;
+                double volumen;
+
+                //este scanner sirve para que el usuaio escriba el radio
+                Scanner sc = new Scanner(System.in);
+                System.out.println("\nEscribe el valor del radio: ");
+                radio = sc.nextDouble();
+
+                //este if hace que si pones un numero que no sea positivo te diga que introduzca valores positivos
+                if (radio > 0 && calculo < 4) {
+                    //aqui estan todos los casos con sus formulas y resultados de las operaciones
+                    switch (calculo) {
+                        case 1:
+                            perimetro = 2 * PI * radio;
+                            System.out.println("\nEl perimetro del círculo es: " + perimetro);
+                            break;
+                        case 2:
+                            area = PI * radio * radio;
+                            System.out.println("\nEl área del círculo es: " + area);
+                            break;
+                        case 3:
+                            volumen = 4 / 3 * PI * radio * radio * radio;
+                            System.out.println("\nEl volumen del circulo es: " + volumen);
+                            break;
+
+                    }
+
+                } else {
+                    System.out.println("Introduzca valores positivos");
+
+                }
+
+            }else {
+                System.out.println("numero incorrecto");
             }
 
-        }while (calculo>0 && calculo<4);
 
+
+        }while (calculo > 0 ) ;
     }
-
 }
 
